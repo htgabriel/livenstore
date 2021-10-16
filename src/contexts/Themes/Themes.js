b983dfe82a75281a1cc1203ff68aba4ec55be03a
Gabriel Henrique <htm.gabriel@gmail.com>
2021-10-16 16:58:50 -0300

    Created Light and Dark theme files

--- src/contexts/Themes/Themes.js
+++ src/contexts/Themes/Themes.js
@@ -1,11 +1,25 @@
 import {createGlobalStyle} from "styled-components";
 
 export const lightTheme = {
-	background: '#ffffff'
+	backgroundPrimary: '#EFEFEF',
+	backgroundSecondary: '#FFFFFF',
+	backgroundTertiary: '#FF000080',
+	backgroundButton: '#48AC64',
+	backgroundButtonInvert: '#3F3F3F',
+	backgroundLowOpacity: '#C4C4C460',
+	text: '#727272',
+	textInvert: '#000000',
 }
 
 export const darkTheme = {
-	background: '#2d2d2d'
+	backgroundPrimary: '#474747',
+	backgroundSecondary: '#1D1D1D',
+	backgroundTertiary: '#FF0000',
+	backgroundButton: '#5ACB79',
+	backgroundButtonInvert: '#3F3F3F',
+	backgroundLowOpacity: '#C4C4C460',
+	text: '#E3E3E3',
+	textInvert: '#FFFFFF',
 }
 
 export const GlobalStyles = createGlobalStyle
@@ -20,10 +34,17 @@ export const GlobalStyles = createGlobalStyle
 		}
 		
 		:root {
-			--background: ${({ theme }) => theme.background}
+			--background-primary: ${({ theme }) => theme.backgroundPrimary};
+			--background-secondary: ${({ theme }) => theme.backgroundSecondary};
+			--background-terciary: ${({ theme }) => theme.backgroundTertiary};
+			--background-button: ${({ theme }) => theme.backgroundButton};
+			--background-button-invert: ${({ theme }) => theme.backgroundButtonInvert};
+			--background-low-opacity: ${({ theme }) => theme.backgroundLowOpacity};
+			--text: ${({ theme }) => theme.text};
+			--text-invert: ${({ theme }) => theme.textInvert};
 		}
 		
 		body {
-			background-color: var(--background);
+			background-color: var(--background-primary);
 		}
 	`
