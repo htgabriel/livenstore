37fa0bcf409d04f7c4ff9bfa991c4883ad1b6eba
Gabriel Henrique <htm.gabriel@gmail.com>
2021-10-20 01:17:52 -0300

    Included Card in components exported list

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Header from "./Header";
 import Search from "./Search";
+import Card from "./Card";
 import CartDropdown from "./CartDropdown";
 
 export {
 	CartDropdown,
 	Header,
-	Search
+	Search,
+	Card
 }
