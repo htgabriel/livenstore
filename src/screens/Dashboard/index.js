ac6ad6d1fdda05fded9967b8bb7bb916045f4392
Gabriel Henrique <htm.gabriel@gmail.com>
2021-10-20 02:20:04 -0300

    Consuming API, mapping products on Dashboard and created pagination

--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styles from './index.module.css'
 import {Card, Header, Search} from '../../components'
+import api from "../../services/api"
 
+const limit_per_page = 8
 function Dashboard(){
+	const [products, setProducts] = useState([])
+	const [page, setPage] = useState(1)
+	
+	useEffect(() => {
+		(async () => {
+			const {data} = await api.get(`api/v1/product`, {
+				params: {
+					page,
+					limit: limit_per_page
+				}
+			})
+			setProducts(data)
+		})()
+	}, [page])
+	
 	return (
 		<div className={styles.container}>
 			<Header />
@@ -11,20 +28,22 @@ function Dashboard(){
 			
 			<div className={styles.dashboard}>
 				<div className={styles.content}>
-					<Card />
-					<Card />
-					<Card />
-					<Card />
-					<Card />
-					<Card />
-					<Card />
-					<Card />
-				</div>
-				
-				<div className={styles.pagination}>
-					<span className={styles.active}/>
-					<span/>
-					<span/>
+					{products.map(product => {
+						return (
+							<Card
+								key={product.id}
+								{...product}
+							/>
+						)
+					})}
+					
+					{page !== 1 &&
+						<span onClick={() => setPage(page -1)}>Pagina Anterior</span>
+					}
+					
+					{products.length === limit_per_page &&
+						<span onClick={() => setPage(page + 1)}>Próxima Página</span>
+					}
 				</div>
 			</div>
 		</div>
