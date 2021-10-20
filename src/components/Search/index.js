c45e44ce8cee2f093d0291240c6df27a987c211f
Gabriel Henrique <htm.gabriel@gmail.com>
2021-10-19 22:00:04 -0300

    Created search component

---
+++ src/components/Search/index.js
@@ -0,0 +1,11 @@
+import styles from './index.module.css'
+
+
+function Search(){
+	return (
+		<div className={styles.content}>
+		</div>
+	)
+}
+
+export default Search
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import Header from "./Header";
+import Search from "./Search";
 import CartDropdown from "./CartDropdown";
 
 export {
 	CartDropdown,
-	Header
+	Header,
+	Search
 }
