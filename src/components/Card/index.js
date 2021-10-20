b6a0cb8237b1c139e7954de85762ac2ca342ff1b
Gabriel Henrique <htm.gabriel@gmail.com>
2021-10-20 02:20:29 -0300

    Consuming API, mapping products on Dashboard and created pagination

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,19 +2,14 @@ import styles from './index.module.css'
 import React from "react";
 import {BiPlus} from "react-icons/all";
 
-function Card(){
+function Card({name, price, image}){
 	return (
 		<div className={styles.card}>
-			<img src="https://site.tim.com.br/sites/default/files/2021-07/meni-noney_0.png" alt="Card img"/>
+			<img src={image} alt="Card img"/>
 			
 			<div className={styles.body}>
-				<span className={styles.product}>
-					Neymar Jr
-				</span>
-				
-				<span>
-					R$500.000.000,00
-				</span>
+				<span className={styles.product}>{name}</span>
+				<span> {`$${price}`} </span>
 			</div>
 			
 			<div className={styles.btndesktop}>
@@ -24,9 +19,7 @@ function Card(){
 			</div>
 			
 			<div className={styles.btnmobile}>
-				<button>
-					Add to cart
-				</button>
+				<button> Add to cart </button>
 			</div>
 		</div>
 	)
