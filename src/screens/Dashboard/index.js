7bec5774fa17671a04d974c4946602be7673fb34
Gabriel Henrique <htm.gabriel@gmail.com>
2021-10-19 22:01:11 -0300

    Created dashboard

--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -1,10 +1,56 @@
 import React from "react";
+import styles from './index.module.css'
+import {Header} from '../../components'
+import {Search} from '../../components'
 
 function Dashboard(){
 	return (
-		<>
-			Dashboard
-		</>
+		<div className={styles.container}>
+			<Header />
+			
+			<Search />
+			
+			<div className={styles.dashboard}>
+				<div className={styles.content}>
+					<div className={styles.card}>
+					
+					</div>
+					
+					<div className={styles.card}>
+					
+					</div>
+					
+					<div className={styles.card}>
+					
+					</div>
+					
+					<div className={styles.card}>
+					
+					</div>
+					
+					<div className={styles.card}>
+					
+					</div>
+					
+					<div className={styles.card}>
+					
+					</div>
+					
+					<div className={styles.card}>
+					
+					</div>
+					<div className={styles.card}>
+					
+					</div>
+				</div>
+				
+				<div className={styles.pagination}>
+					<span className={styles.active}/>
+					<span/>
+					<span/>
+				</div>
+			</div>
+		</div>
 	)
 }
 
