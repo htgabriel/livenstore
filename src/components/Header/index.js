06b29bdd2e306a58fb1cf83755369df40c249ed9
Gabriel Henrique <htm.gabriel@gmail.com>
2021-10-26 02:14:40 -0300

    Added menu mobile, icon to switch theme

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,29 +15,44 @@ function Header(){
 	return (
 		<div className={styles.header}>
 			<div className={styles.content}>
+				<input type="checkbox" id="menu" className={styles.ckboxMenu}/>
+				<label className={styles.hamburguer} htmlFor="menu">
+					<span/>
+					<span/>
+					<span/>
+				</label>
+				
 				<div className={styles.menu}>
 					<div>SHOP</div>
 					<div>PRODUCTS</div>
+					<div> LOGIN </div>
 				</div>
 				
 				<div className={styles.logo}>
 					LIVEN STORE
 				</div>
 				
-				<div className={styles.relative}>
-					<MdOutlineShoppingCart
-						size={35}
-						onClick={toggleDropdown}
-						className={styles.pointer}
-					/>
+				<div className={styles.flex}>
+					<div className={styles.themeButton}>
+						{theme === 'dark' && <BsSun size={25} onClick={() => toggleTheme()} />}
+						{theme === 'light' && <BiMoon size={25} onClick={() => toggleTheme()} />}
+					</div>
 					
-					<span className={styles.productsInCart}>
+					<div className={styles.cart}>
+						<MdOutlineShoppingCart
+							size={35}
+							onClick={toggleDropdown}
+							className={styles.pointer}
+						/>
+						
+						<span className={styles.productsInCart}>
 						{productsInCart.length}
 					</span>
-					
-					<CartDropdown
-						isVisible={cartVisible}
-					/>
+						
+						<CartDropdown
+							isVisible={cartVisible}
+						/>
+					</div>
 				</div>
 			</div>
 		</div>
