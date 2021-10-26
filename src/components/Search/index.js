b21b13a4c4a45c8e68502850b88f4e0bb3643f22
Gabriel Henrique <htm.gabriel@gmail.com>
2021-10-26 02:16:56 -0300

    Added search

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,9 +1,17 @@
 import styles from './index.module.css'
+import {GoSearch} from "react-icons/all";
 
-
-function Search(){
+function Search({onSearch}){
 	return (
 		<div className={styles.content}>
+			<input
+				type="text"
+				onBlur={({target: {value}}) => onSearch && onSearch(value)}
+			/>
+			
+			<button className={styles.btnSearch}>
+				<GoSearch size={20} />
+			</button>
 		</div>
 	)
 }
